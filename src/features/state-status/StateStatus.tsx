3f0bcd56320b6f0e9fd1718f2f9db8c8af671f4e
Sergey Laletin <Quercue@gmail.com>
2026-04-09 21:14:09 +0300

    create state status component

---
+++ src/features/state-status/StateStatus.tsx
@@ -0,0 +1,12 @@
+export const StateStatus = () => {
+  return (
+    <div className="flex flex-col gap-1">
+      <h1 className="text-4xl text-[#d09683] font-extrabold leading-tight">
+        В ПОТОКЕ
+      </h1>
+      <p className="text-m text-[#73605b] font-extralight">
+        Ты удерживаешь путь
+      </p>
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
+import { StateStatus } from "../../features/state-status/StateStatus";
+
 export const HomePage = () => {
   return (
     <div className="h-screen w-screen bg-[#363237] flex flex-col items-center justify-center">
       <div className="flex flex-col gap-1">
-        <h1 className="text-4xl text-[#d09683] font-extrabold leading-tight mb-4">
+        <h1 className="text-4xl text-[#d09683] font-extrabold leading-tight">
           pathkeeper
         </h1>
+        <StateStatus />
         <p className="text-m text-[#73605b] font-extralight">
           Твой путь начинается здесь
         </p>
