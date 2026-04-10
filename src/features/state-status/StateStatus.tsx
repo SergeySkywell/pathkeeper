3675f0d8311d4f0b380f8022c0a22e811c59b153
Sergey Laletin <Quercue@gmail.com>
2026-04-10 20:56:17 +0300

    replaced the hardcode with the state

--- src/features/state-status/StateStatus.tsx
+++ src/features/state-status/StateStatus.tsx
@@ -1,8 +1,12 @@
+import { useStore } from "../../app/store/useStore";
+
 export const StateStatus = () => {
+  const state = useStore((state) => state.state);
+
   return (
     <div className="flex flex-col gap-1">
       <h1 className="text-4xl text-[#d09683] font-extrabold leading-tight">
-        В ПОТОКЕ
+        {state}
       </h1>
       <p className="text-m text-[#73605b] font-extralight">
         Ты удерживаешь путь
