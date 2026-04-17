69d30d46f09d594b5703d7ddd145f78b0e1129a2
Sergey Laletin <Quercue@gmail.com>
2026-04-17 18:45:00 +0300

    Get Stats from Store

--- src/app/store/useStore.ts
+++ src/app/store/useStore.ts
@@ -4,10 +4,24 @@ type StateType = "lost" | "unstable" | "stable" | "flow" | "deep";
 
 interface State {
   state: StateType;
+  stats: {
+    attention: number;
+    energy: number;
+    discipline: number;
+    clarity: number;
+    depth: number;
+  };
   setState: (newState: StateType) => void;
 }
 
 export const useStore = create<State>((set) => ({
   state: "flow",
+  stats: {
+    attention: 10,
+    energy: 10,
+    discipline: 10,
+    clarity: 10,
+    depth: 10,
+  },
   setState: (newState: StateType) => set({ state: newState }),
 }));
--- src/features/stats/StatsList.tsx
+++ src/features/stats/StatsList.tsx
@@ -1,21 +1,43 @@
 import { StatItem } from "./StatItem";
+import { useStore } from "../../app/store/useStore";
+
+type StatEntry = {
+  name: string;
+  value: number;
+};
 
 export const StatsList = () => {
-  const stats = [
-    { name: "Внимание", value: 10 },
-    { name: "Энергия", value: 10 },
-    { name: "Дисциплина", value: 10 },
-    { name: "Ясность", value: 10 },
-    { name: "Глубина", value: 10 },
-  ];
+  const stats = useStore((state) => state.stats);
+
+  const statsArray = Object.entries(stats).map(
+    ([name, value]): StatEntry => ({
+      name,
+      value,
+    }),
+  );
+
+  const statsLabels: Record<string, string> = {
+    attention: "Внимание",
+    energy: "Энергия",
+    discipline: "Дисциплина",
+    clarity: "Ясность",
+    depth: "Глубина",
+  };
+
+  /* РЕНДЕР */
+
   return (
     <div className="flex flex-col gap-3">
       <h1 className="text-4xl text-[#bb6e26] font-extrabold leading-tight">
         Характеристики
       </h1>
       <ul>
-        {stats.map((stat) => (
-          <StatItem key={stat.name} name={stat.name} value={stat.value} />
+        {statsArray.map((stat) => (
+          <StatItem
+            key={stat.name}
+            name={statsLabels[stat.name] || stat.name}
+            value={stat.value}
+          />
         ))}
       </ul>
     </div>
