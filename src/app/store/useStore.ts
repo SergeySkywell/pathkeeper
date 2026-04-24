3dd783327ada2a803acf4ecefaed74254fbfd5d9
Sergey Laletin <Quercue@gmail.com>
2026-04-24 18:11:10 +0300

    feat: add updateStat action to store

--- src/app/store/useStore.ts
+++ src/app/store/useStore.ts
@@ -3,14 +3,23 @@ import type { Stats } from "@/shared/lib/types";
 
 interface PathkeeperStore {
   stats: Stats;
+  updateStat: (name: keyof Stats, delta: number) => void;
 }
 
-export const useStore = create<PathkeeperStore>(() => ({
+export const useStore = create<PathkeeperStore>((set) => ({
   stats: {
     attention: 10,
     energy: 10,
     discipline: 10,
     clarity: 10,
     depth: 10,
+  },
+  updateStat: (name, delta) => {
+    set((state) => ({
+      stats: {
+        ...state.stats,
+        [name]: state.stats[name] + delta,
+      },
+    }));
   },
 }));
